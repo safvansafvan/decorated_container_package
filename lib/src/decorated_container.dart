d81f75213d0385e4e443751627d7ad92349eecae
muhammed safvan <sajusajuptl@gmail.com>
2023-09-19 19:16:48 +0530

    finished

--- lib/src/decorated_container.dart
+++ lib/src/decorated_container.dart
@@ -1,8 +1,20 @@
-import 'package:flutter/foundation.dart';
-import 'package:flutter/widgets.dart';
+import 'package:flutter/material.dart';
 
 class DecoratedContainer extends StatefulWidget {
-  const DecoratedContainer({super.key});
+  const DecoratedContainer(
+      {super.key,
+      this.width,
+      this.height,
+      this.color1,
+      this.color2,
+      this.title,
+      this.subtitle});
+  final double? width;
+  final double? height;
+  final Color? color1;
+  final Color? color2;
+  final String? title;
+  final String? subtitle;
 
   @override
   State<DecoratedContainer> createState() => _DecoratedContainerState();
@@ -11,6 +23,33 @@ class DecoratedContainer extends StatefulWidget {
 class _DecoratedContainerState extends State<DecoratedContainer> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Container(
+      width: widget.width,
+      height: widget.height,
+      decoration: BoxDecoration(
+          gradient: LinearGradient(colors: [
+            widget.color1 ?? Colors.blue,
+            widget.color2 ?? Colors.amber
+          ]),
+          boxShadow: const [BoxShadow(color: Colors.grey, blurRadius: 12)]),
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Text(
+              widget.title ?? "Unknown",
+              style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(height: 10),
+            Text(
+              widget.subtitle ?? "",
+              style: const TextStyle(fontSize: 17, fontWeight: FontWeight.bold),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
